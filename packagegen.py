31eec653957c67dc75563a73b2cfd2c84ee7f90c
Jakob Kristensen <j4k0bk@gmail.com>
2016-10-20 15:03:03 +0200

    Added Python script to generate packages.txt

---
+++ packagegen.py
@@ -0,0 +1,40 @@
+import random
+
+
+NUM_PACKAGES = 10**3
+PACKAGE_TYPES = [
+    'LegalPackage',
+    'Hash',
+    'Heroin',
+    'DonaldTrumpMerchandise',
+]
+PACKAGE_DESTINATIONS = [
+    'DEN',
+    'GER',
+    'GBR',
+    'FRA',
+    'USA',
+    'RHO',
+    'SPA',
+    'RUS',
+    'SWE',
+]
+
+
+def create_package():
+    package_type = random.choice(PACKAGE_TYPES)
+    destination = random.choice(PACKAGE_DESTINATIONS)
+    weight = random.randint(10, 100)
+
+    return package_type, destination, str(weight)
+
+
+def generate_package_list(tofile):
+    for i in range(NUM_PACKAGES):
+        package = create_package()
+        tofile.write('%s\n' % ' '.join(package))
+
+
+if __name__ == '__main__':
+    with open('packages.txt', 'w') as tofile:
+        generate_package_list(tofile)
